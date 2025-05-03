c124ecccc0d006ebced3f5e48dedd23d30e7143c
Bob Li <bobli8203@gmail.com>
2025-05-03 14:31:09 -0400

    added residual and normalization layers

--- src/transformer_lemmatization/encoding_layers.py
+++ src/transformer_lemmatization/encoding_layers.py
@@ -10,12 +10,13 @@ class position_wide_feed_forward(nn.Module):
         '''
         A Constructor for the positional feed forward network
         dimensin_for_model: the manually decided dimension that's used in the embeddings layer
-        dimension_for_network: the dimension needed for the 
+        dimension_for_network: the dimension needed to expand the embedded results into
+        dropout: optional dropout to wipe out specific columns and rows of the matrix to improve the model's abilities during training
         '''
 
         super.__init__() #initializing the parent class - 'neural-networks'
         self.expansion = nn.Linear(dimension_for_model, dimension_for_network) #expanding the original batch taken from the multi-head attention into newer ones with the desired dimensions
-        self.dropout = nn.Dropout(dropout) #creating the dropout layer for improving the model's ability through testing and training by replacing specific rows and columns with 0s
+        self.apply_dropout = nn.Dropout(dropout) #creating the dropout layer for improving the model's ability through testing and training by replacing specific rows and columns with 0s
         self.activation = nn.ReLU() #introducing non-linearity into the encoder and allowing models to represent values non-linearly
         self.reverse_expansion = nn.Linear(dimension_for_network, dimension_for_model) # reducing the dimension from expanded into original
     def forward(self, x):
@@ -24,14 +25,21 @@ class position_wide_feed_forward(nn.Module):
         Applying the process of the positional feed_forward function
         x: the data which the positional feed forward is applied to
         '''
-        parsed = self.dropout(self.activation(self.expansion(x)))
+        parsed = self.apply_dropout(self.activation(self.expansion(x)))
         return self.reverse_expansion(parsed)
 #using the layer_normalization to add outputs back and then normalize the layer
 
-class Layer_normalization (nn.Module):
-    def __init__(self, dimension_for_model, dropout = 0.1):
-        print()
-    def forward(self, mask = None):
-        print()
+class Residual_layer (nn.Module):
+    def __init__(self, dropout = 0.1):
+        '''
+        A Constructor for the Residual and Normalization Layer
+        dropout: optional dropout to wipe out specific columns and rows of the matrix to improve the model's abilities during training
+        '''
+        self.normalize = nn.LayerNorm()  #creating the layer normalization
+        self.apply_dropout = nn.Dropout(dropout)
+    def forward(self, parsed, attention_result):
+        result = parsed+attention_result  #adding two results together, since both are of same dimension to enforce the positional arguments
+        return self.normalize(result) #return the normalized result
+        
 
 
